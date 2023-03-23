5862ebeb5483f67dcf629e05d191a4cb2bacf7dd
Ibrahim <ibrahimseda322@gmail.com>
2023-03-23 15:22:49 +0200

    added the homepage arrow

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,15 @@ import Container from "react-bootstrap/Container";
 import AnimatedTitle from "./AnimatedTitle";
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 function Home(){
     return  <Container id="home" className="my-home-page" fluid>
-    <Row className="my-4"><p>Welcome</p></Row>
-    <Row className="my-4"><AnimatedTitle /></Row>
-    <Row className="my-4"><p>based in Cairo, Egypt.</p></Row>
-    <Row className="mt-4 "><Button className="rounded-pill" variant="outline-success">Hire Me</Button></Row>
+    <Row className="mt-auto p-4"><p>Welcome</p></Row>
+    <Row className="p-4"><AnimatedTitle /></Row>
+    <Row className="p-4"><p>based in Cairo, Egypt.</p></Row>
+    <Row className="p-4"><Button className="rounded-pill" variant="outline-success">Hire Me</Button></Row>
+    <Row className="mb-0 mt-auto"><a><KeyboardArrowDownIcon className="arrow bounce" /></a></Row>
   </Container> 
   ;
 }
