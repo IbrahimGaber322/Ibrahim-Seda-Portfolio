6430a2cac9ab0bbb1b811e60ecd14bcdc87dd72c
IbrahimGaber322 <ibrahimseda322@gmail.com>
2023-07-29 19:24:43 +0300

    loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import Container from "react-bootstrap/Container";
@@ -12,10 +12,45 @@ import Portfolio from "./pages/Portfolio";
 import Testimonial from "./pages/Testimonial";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
+import background from "./images/back-ground.jpg";
+import backgroundMob from "./images/back-ground-mob.jpg";
 
 function App() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const updateScreenWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", updateScreenWidth);
+
+    return () => {
+      window.removeEventListener("resize", updateScreenWidth);
+    };
+  }, []);
+  const backgroundImage = screenWidth > 576 ? background : backgroundMob;
+
+  const [loading, setLoading] = useState(true);
+  const handleBackgroundLoad = () => {
+    setLoading(false); 
+  };
+
   return (
-    <Container className="my-app" fluid>
+    <Container
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.365), black), url(${backgroundImage}`,
+      }}
+      className="my-app"
+      fluid
+    >
+      <img
+        src={backgroundImage}
+        alt="Background"
+        style={{ display: "none" }} 
+        onLoad={handleBackgroundLoad}
+      />
+      {loading&&<div className="loader-background"><div className="loader">Loading...</div></div>}
       <Header />
       <Home />
       <About />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,48 @@
 import React from "react";
 import AnimatedTitle from "../components/AnimatedTitle";
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col  from "react-bootstrap/Col";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
-function Home(){
-    return  <section className="container-fluid my-home-page" id="home">
-    <Row className="mt-auto p-4"><Col><p>Welcome</p></Col></Row>
-    <Row className="p-4"><Col><AnimatedTitle /></Col></Row>
-    <Row className="p-4"><Col><p>based in Cairo, Egypt.</p></Col></Row>
-    <Row className="p-4"><Col><Button href="#contact" className="rounded-pill" variant="outline-success">Hire Me</Button></Col></Row>
-    <Row className="mb-0 mt-auto"><Col><a href="#about"><KeyboardArrowDownIcon className="arrow bounce" /></a></Col></Row>
-  </section>
+function Home() {
+  return (
+    <section className="container-fluid my-home-page" id="home">
+      <Row className="mt-auto p-4">
+        <Col>
+          <p>Welcome</p>
+        </Col>
+      </Row>
+      <Row className="p-4">
+        <Col>
+          <AnimatedTitle />
+        </Col>
+      </Row>
+      <Row className="p-4">
+        <Col>
+          <p>based in Cairo, Egypt.</p>
+        </Col>
+      </Row>
+      <Row className="p-4">
+        <Col>
+          <Button
+            href="#contact"
+            className="rounded-pill"
+            variant="outline-success"
+          >
+            Hire Me
+          </Button>
+        </Col>
+      </Row>
+      <Row className="mb-0 mt-auto">
+        <Col>
+          <a href="#about">
+            <KeyboardArrowDownIcon className="arrow bounce" />
+          </a>
+        </Col>
+      </Row>
+    </section>
+  );
 }
 
 export default Home;
