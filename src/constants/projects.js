9e4328d22458f15549325c5b2e0aeb482ee61f6e
IbrahimGaber322 <ibrahimseda322@gmail.com>
2024-10-10 14:01:46 +0300

    Clean unused images

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,14 +1,8 @@
 import WebWeave from "../images/WebWeave gif.gif";
-/* import bitnine from "../images/bitnine.jpg"; */
-/* import alam from "../images/3alam.gif"; */
-/* import reiz from "../images/reiz.gif"; */
 import Threads from "../images/Threads.gif";
 import Tasks from "../images/Tasks.gif";
-/* import JavaScript from "../images/javascript.gif"; */
 import Knot from "../images/knot.gif";
 import Instatus from "../images/instatus.gif";
-/* import Technoboly from "../images/Technoboly.gif"; */
-/* import Montag from "../images/Montag.webp"; */
 import Bazaar from "../images/Bazaar.gif";
 import GoodReads from "../images/GoodReads.gif";
 const projects = [
@@ -29,47 +23,6 @@ const projects = [
     visitLink: "https://webweave.onrender.com/",
     category: "React + Nodejs",
   },
-/*   {
-    name: "Bitnine",
-    imgSrc: bitnine,
-    keyFeatures: [
-      "Developed bilingual single-page website with captivating animations using React.",
-      "Created RESTful API server with Express and PostgreSQL, including user authentication.",
-      "Utilized essential packages for security and hosted PostgreSQL on Render.com.",
-      "Conducted comprehensive testing for reliable functionality.",
-    ],
-    description:
-      "Visit Bitnine and explore the captivating animations and functionality of this website.",
-    visitLink: "https://bitnine.onrender.com/",
-    category: "React + Nodejs",
-  }, */
-  /*  {
-    name: "3alam",
-    imgSrc: alam,
-    keyFeatures: [
-      "Developed a unique and attractive company website using Next.js, React, and TypeScript.",
-      "Utilized server-side rendering for performance and SEO benefits.",
-      "Incorporated smooth animations and interactive elements for an enhanced user experience.",
-      "Implemented Material-UI icons and components for a consistent UI design.",
-    ],
-    description:
-      "Explore the attractive design and interactive elements of the 3alam company website.",
-    visitLink: "https://next-3r88-6pva73934-ibrahimgaber322.vercel.app/",
-    category: ["NextJs"],
-  }, */
-/*   {
-    name: "Countries",
-    imgSrc: reiz,
-    keyFeatures: [
-      "Created a React web app for sorting countries by size, continent, and alphabetical order.",
-      "Utilized Material-UI and Emotion CSS-in-JS for a responsive UI.",
-      "Integrated restcountries.com API to fetch country data.",
-    ],
-    description:
-      "Explore the features and sorting functionality of the Countries web app.",
-    visitLink: "https://countries-ghr6.onrender.com/",
-    category: "React",
-  }, */
   {
     name: "Threads",
     imgSrc: Threads,
@@ -100,19 +53,6 @@ const projects = [
     visitLink: "https://tasks-kabg.onrender.com/",
     category: "React + Nodejs",
   },
- /*  {
-    name: "JavaScript Functions",
-    imgSrc: JavaScript,
-    keyFeatures: [
-      "Explore age calculation, even/odd guessing, discount calculation, and more.",
-      "Frontend: Built using React.js and styled with Material-UI.",
-      "Navigate easily and interact intuitively.",
-      "Encapsulate each function for code reusability and maintenance.",
-    ],
-    visitLink: "https://iti-javascript.vercel.app/",
-    description: "Visit now and try different JavaScript functions!",
-    category: "React",
-  }, */
   {
     name: "KNOT (Link Manager)",
     imgSrc: Knot,
@@ -143,35 +83,6 @@ const projects = [
     description: "Visit now and check this amazing tool !",
     category: "NextJs",
   },
-/*   {
-    name: "Technoboly",
-    imgSrc: Technoboly,
-    keyFeatures: [
-      "E-commerce functionality with Elementor and WooCommerce.",
-      "Google Maps integration.",
-      "Dual-language support.",
-      "Wide variety of acrylic & polycarbonate products.",
-      "Frequently visited.",
-    ],
-    description:
-      "Explore our e-commerce website with a wide range of products.",
-    visitLink: "https://technoboly.com/",
-    category: "WordPress",
-  }, */
-/*   {
-    name: "Montag",
-    imgSrc: Montag,
-    keyFeatures: [
-      "User Authentication and Authorization",
-      "Product Browsing and Viewing",
-      "Shopping Cart Functionality",
-      "Seamless Checkout Process",
-    ],
-    description:
-      "Montag is a simple ecommerce website developed using Angular, Angular Material, and Angular Bootstrap. Explore its features for a user-friendly online shopping experience.",
-    visitLink: "https://montag-client.vercel.app/home",
-    category: "Angular",
-  }, */
   {
     name: "Bazaar",
     imgSrc: Bazaar,
